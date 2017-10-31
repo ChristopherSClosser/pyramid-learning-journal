1042fa9caf97b77823679999c6cc29f242d86832
Chris Closser <chrisclosser1@gmail.com>
2017-10-30 21:19:01 -0700

    something is working

--- pyramid_learning_journal/pyramid_learning_journal/__init__.py
+++ pyramid_learning_journal/pyramid_learning_journal/__init__.py
@@ -5,6 +5,7 @@ from pyramid.config import Configurator
 def main(global_config, **settings):
     """Return a Pyramid WSGI application."""
     config = Configurator(settings=settings)
+    config.add_static_view(name='static', path='static')
     config.include('.views')
     config.include('pyramid_jinja2')
     config.include('.routes')
--- pyramid_learning_journal/pyramid_learning_journal/views/__init__.py
+++ pyramid_learning_journal/pyramid_learning_journal/views/__init__.py
@@ -2,6 +2,7 @@
 
 from .default import list_view
 
+
 def includeme(config):
     """List of views to include for the configurator object."""
     config.add_view(list_view, route_name='home')
