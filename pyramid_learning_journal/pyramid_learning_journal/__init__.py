37bde8ba18ea1be4b30832b9a94109fa84d6b374
Chris Closser <chrisclosser1@gmail.com>
2017-11-05 22:02:10 -0800

    css

--- pyramid_learning_journal/pyramid_learning_journal/__init__.py
+++ pyramid_learning_journal/pyramid_learning_journal/__init__.py
@@ -5,10 +5,11 @@ from pyramid.config import Configurator
 def main(global_config, **settings):
     """Return a Pyramid WSGI application."""
     config = Configurator(settings=settings)
-    config.add_static_view(name='static', path='static')
+    config.add_static_view(name='static', path='pyramid_learning_journal:static')
     config.include('.views')
     config.include('pyramid_jinja2')
     config.include('.routes')
     config.include('.models')
+    # config.include('.static')
     config.scan()
     return config.make_wsgi_app()
