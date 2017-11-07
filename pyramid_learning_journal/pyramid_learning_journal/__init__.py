794283a2f5e0e3c98d042198535faf5667a5bbee
Chris Closser <chrisclosser1@gmail.com>
2017-11-06 16:40:29 -0800

    db env var set and working

--- pyramid_learning_journal/pyramid_learning_journal/__init__.py
+++ pyramid_learning_journal/pyramid_learning_journal/__init__.py
@@ -1,15 +1,16 @@
 """."""
 from pyramid.config import Configurator
+import os
 
 
 def main(global_config, **settings):
     """Return a Pyramid WSGI application."""
+    settings["sqlalchemy.url"] = os.environ["DATABASE_URL"]
     config = Configurator(settings=settings)
     config.add_static_view(name='static', path='pyramid_learning_journal:static')
     config.include('.views')
     config.include('pyramid_jinja2')
     config.include('.routes')
     config.include('.models')
-    # config.include('.static')
     config.scan()
     return config.make_wsgi_app()
