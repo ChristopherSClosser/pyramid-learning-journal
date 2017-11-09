dd9f6254e076c2231095d35e7bbac83be72d764b
Brendan Davis <brendanmd@gmail.com>
2017-11-09 14:43:14 -0800

    some tests

--- pyramid_learning_journal/pyramid_learning_journal/tests.py
+++ pyramid_learning_journal/pyramid_learning_journal/tests.py
@@ -1,4 +1,4 @@
-"""."""
+"""For testing the pyramid learning journal."""
 
 from datetime import datetime
 from pyramid_learning_journal.models import MyModel
@@ -11,13 +11,13 @@ from pyramid_learning_journal.views.default import (
 
 
 def test_home_route_get_request_200_ok(testapp):
-    """."""
+    """Home route == 200"""
     response = testapp.get('/')
     assert response.status_code == 200
 
 
 def test_home_route_get_no_entries_has_no_sections(testapp):
-    """."""
+    """Ain't no children there."""
     response = testapp.get('/')
     html = response.html
     content = html.find_all('content')[0]
@@ -25,7 +25,7 @@ def test_home_route_get_no_entries_has_no_sections(testapp):
 
 
 def test_home_route_get_no_entries_has_sections(testapp, fill_my_database):
-    """."""
+    """One section."""
     response = testapp.get('/')
     html = response.html
     content = html.find_all('section')
@@ -33,7 +33,7 @@ def test_home_route_get_no_entries_has_sections(testapp, fill_my_database):
 
 
 def test_home_route_with_many_entries_has_sections(testapp, fill_my_other_database):
-    """."""
+    """Tons of entries!"""
     response = testapp.get('/')
     html = response.html
     content = html.find_all('section')
@@ -48,7 +48,7 @@ def test_create_view_post_empty_is_empty_dict(dummy_request):
 
 
 def test_home_with_one_entry(testapp, fill_my_database):
-    """."""
+    """Home has one entry"""
     response = testapp.get('/')
     html = response.html
     content = html.find_all('section')
@@ -56,7 +56,7 @@ def test_home_with_one_entry(testapp, fill_my_database):
 
 
 def test_home_with_many_entries(testapp, fill_my_other_database):
-    """."""
+    """Home has many entries!!"""
     response = testapp.get('/')
     html = response.html
     content = html.find_all('section')
@@ -64,6 +64,6 @@ def test_home_with_many_entries(testapp, fill_my_other_database):
 
 
 def test_add_new_entry_200(testapp, dummy_request):
-    """."""
+    """New entry == 200"""
     dummy_request.method = testapp.get('/journal/new-entry')
     assert dummy_request.response.status == '200 OK'
