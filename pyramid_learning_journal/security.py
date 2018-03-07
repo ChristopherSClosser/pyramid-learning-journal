0d6a662553664ba5bb72fcb1c593e1d22e199771
Chris Closser <chrisclosser1@gmail.com>
2018-03-07 08:03:30 -0800

    working on authentication and authorization

--- pyramid_learning_journal/security.py
+++ pyramid_learning_journal/security.py
@@ -1,12 +1,12 @@
-# """Security configuration for our Pyramid application."""
-#
-# import os
-# from pyramid.authentication import AuthTktAuthenticationPolicy
-# from pyramid.authorization import ACLAuthorizationPolicy
-# from pyramid.security import Everyone, Authenticated
-# from pyramid.security import Allow
-# from passlib.apps import custom_app_context as pwd_context
-#
+"""Security configuration for Pyramid application."""
+
+import os
+from pyramid.authentication import AuthTktAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
+from pyramid.security import Everyone, Authenticated
+from pyramid.security import Allow
+from passlib.apps import custom_app_context as pwd_context
+
 #
 # def check_credentials(username, password):
 #     """."""
@@ -38,15 +38,15 @@
 #     ]
 #
 #
-# def includeme(config):
-#     """Security-related configuration."""
-#     auth_secret = os.environ.get('AUTH_SECRET', 'itsaseekrit')
-#     authn_policy = AuthTktAuthenticationPolicy(
-#         secret=auth_secret,
-#         hashalg='sha512'
-#     )
-#     config.set_authentication_policy(authn_policy)
-#     authz_policy = ACLAuthorizationPolicy()
-#     config.set_authorization_policy(authz_policy)
-#     config.set_default_permission('view')
-#     config.set_root_factory(MyRoot)
+def includeme(config):
+    """Security-related configuration."""
+    auth_secret = os.environ.get('AUTH_SECRET', '')
+    authn_policy = AuthTktAuthenticationPolicy(
+        secret=auth_secret,
+        hashalg='sha512'
+    )
+    config.set_authentication_policy(authn_policy)
+    authz_policy = ACLAuthorizationPolicy()
+    config.set_authorization_policy(authz_policy)
+    config.set_default_permission('view')
+    config.set_root_factory(MyRoot)
